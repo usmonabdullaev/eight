3cec66992222d47fa7ac5cc7a7ed9c66d9dbb282
Abdullaev Usmon <abdullaevusmon2006@gmail.com>
2024-03-08 09:03:26 +0500

    Update animation

--- src/components/Mobile.jsx
+++ src/components/Mobile.jsx
@@ -1,12 +1,9 @@
-import image1 from "../image1.jpg";
-
 const Mobile = () => {
   return (
     <div className="mobile">
       <h1 className="mobile__title">
         Гулдухтарони синфи 11'А'-ро бо иди 8-уми март табрик менамоям...
       </h1>
-      {/* <img src={image1} alt="8-march" className="mobile__img" /> */}
     </div>
   );
 };
